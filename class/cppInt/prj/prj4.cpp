6e90567f160850eca4bcf789f8e67a4a315b9ca9
jr.kim <jr.kim@lge.com>
2024-10-25 14:56:17 +0900

    add cppInt #7

--- class/cppInt/prj/prj4.cpp
+++ class/cppInt/prj/prj4.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 class People
@@ -16,7 +17,7 @@ public:
 	{
 		cout << " Copy Ctor" << endl;
 	}
-	People(People&& other) : name{move(other.name)}, addr{move(other.addr)}, age{other.age}
+	People(People&& other) noexcept : name{move(other.name)}, addr{move(other.addr)}, age{other.age}
 	{
         other.age = 0;
 		cout << " Move Ctor" << endl;
@@ -39,15 +40,32 @@ public:
 		cout << " Move =" << endl;
         return *this;
 	}
+    ~People() { std::cout << "~People()" << std::endl; }
+    string gettInfoStr()
+    {
+        ostringstream oss;
+        oss << name << ", " << addr << ", " << age;
+        return oss.str();
+    }
 };
 
 int main()
 {
     // 3번 과제에서 만든 People를 사용하세요.
 
-    // 1. People를 힙에 10 생성해 보세요.
+    // 1. People를 힙에 10개 생성해 보세요.
     //    People{"홍길동","서울",20} 으로 초기화 하세요.
     //    힌트) 메모리 할당과 생성자 호출을 분리 하세요.
+
+    int n1 = 10;
+	People* p1 = static_cast<People*>(operator new(sizeof(People) * n1));
+
+	for (int i = 0; i < n1; i++)
+	{
+    	new(&p1[i]) People("홍길동","서울",20);
+	}
+
+
 
     // 2. 위에서 만드는 배열(버퍼)를 20개 키워 보세요.
     //     A. 20개 버퍼를 새로 할당하고
@@ -56,7 +74,30 @@ int main()
     //         예외가 있다면 복사 생성자로 옮기세요.
     //     C. 버퍼의 새로운 공간은 {"unknown", "unknown", 0} 으로 초기화 하세요.
 
+    int n2 = 20;
+	People* p2 = static_cast<People*>(operator new(sizeof(People) * n2)) ;
+
+	for (int i = 0; i < n2; i++)
+	{
+        if (i < n1)
+        {
+            new(&p2[i]) People(std::move_if_noexcept( p1[i] ));
+        }
+        else 
+        {
+            new(&p2[i]) People("unknown","unknow",0);
+        }
+	}
+
     // 3. 새로운 버퍼의 내용을 출력하세요.
+    for (int i = 0; i < n2; i++) 
+    {
+        cout << i << ", " << p2[i].gettInfoStr() << endl;
+    }
 
     // 4. 버퍼을 줄이지는 말고 버퍼 끝에 있는 3개 객체를 파괴(명시적 소멸자 호출)해 보세요.
+    for (int i = 0; i < 3; i++)
+    {
+        p2[n2 - i - 1].~People();
+    }
 }
--- class/cppInt/prj/prj5.cpp
+++ class/cppInt/prj/prj5.cpp
@@ -3,6 +3,12 @@
 // 아래 Point 에는 디폴트 생성자가 없습니다. 디폴트 생성자를 만들지 말고,
 // 아래 상태에 과제를 진행해 주세요.
 
+#include <cstring>
+#include <iostream>
+#include <initializer_list>
+
+using namespace std;
+
 class Point
 {
 public:
@@ -16,6 +22,66 @@ template<typename T> class Vector
     int sz;     // size
     int capa;   // capacity ( 메모리 사용량 )
 public:
+	Vector(int n, const T& other) : sz(n), capa(n)
+    {  
+        T* buff = static_cast<T*>(operator new(sizeof(T) * sz));
+        for (int i=0; i<sz; ++i)
+        {
+            memcpy(&buff[i], &other, sizeof(T));
+        }
+    }
+    // Vector(std::initializer_list<T> s)
+    // {
+    //     sz = s.size();
+    //     capa = sz;
+    //     T* buff = static_cast<T*>(operator new(sizeof(T) * sz));
+    //     int index = 0;
+    //     for(auto i : s)
+    //     {
+    //         buff[index] = i;
+    //         ++index;
+    //     }
+    // }
+    void resize(int n, const T& other=T(0,0))
+    {  
+        if (n <= sz)
+        {
+           for (int i=0; i<sz-n;++i)
+            {
+                buff[sz-i-1].~T();
+            }
+
+            sz = n;
+        } 
+        else
+        {
+
+            T* new_buff = static_cast<T*>(operator new(sizeof(T)*n));
+
+            for (int i=0; i<sz; ++i)
+            {
+                memmove(&new_buff[i], &other, sizeof(T));
+            }
+
+            for (int i=0; i<n-sz; ++i)
+            {
+                memcpy(&new_buff[sz+i], &other, sizeof(T));
+            }
+
+            for (int i=0; i<sz;++i)
+            {
+                buff[i].~T();
+            }
+
+            delete[] buff;
+
+            buff = new_buff;
+            sz = n;
+            capa = n;
+        }
+    }
+    int size() { return sz;}
+    int capacity() { return capa; }
 };
 
 int main()
@@ -35,9 +101,9 @@ int main()
     cout << v.capacity() << endl;   // 20
 
     // 4. initializer_list 추가하기.
-    Vector<int> v2{ 1,2,3,4,5,6,7,8,9,10 };
+    // Vector<int> v2{ 1,2,3,4,5,6,7,8,9,10 };
 
     // 5. ranged for 지원하기
-    for (auto n : v2)
-        cout << n << endl; // 1,2,3,4,5,6,7,8,9,10
+    // for (auto n : v2)
+    //     cout << n << endl; // 1,2,3,4,5,6,7,8,9,10
 }
